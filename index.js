38932bcde5c5b56a9176ab8d606feda8db93b2c1
workingjubilee <workingjubilee@gmail.com>
2019-03-22 08:11:29 -0700

    Initial commit: It hungers.

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 // play this: https://www.youtube.com/watch?v=d-diB65scQU
 
 // code away!
+
+const express = require("express");
+
+const server = express();
+
+server.listen(4000, () => {
+  console.log('\n*** BEWARE! I HUNGER! ***\n');
+});
