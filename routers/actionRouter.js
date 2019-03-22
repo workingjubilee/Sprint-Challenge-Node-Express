6af8c5a7220a47a92133aadeaae493833f6e3974
workingjubilee <workingjubilee@gmail.com>
2019-03-22 08:46:13 -0700

    Skeletonize router structure

---
+++ routers/actionRouter.js
@@ -0,0 +1,28 @@
+const express = require("express");
+
+const Actions = require('../data/helpers/projectModel.js')
+
+const router = express.Router();
+
+
+| Field       | Data Type | Metadata                                                                                         |
+| ----------- | --------- | ------------------------------------------------------------------------------------------------ |
+| id          | number    | no need to provide it when creating posts, the database will automatically generate it.          |
+| project_id  | number    | required, must be the id of an existing project.                                                 |
+| description | string    | up to 128 characters long, required.                                                             |
+| notes       | string    | no size limit, required. Used to record additional notes or requirements to complete the action. |
+| completed   | boolean   | used to indicate if the action has been completed, not required                                  |
+
+// Crud
+router.post();
+
+// cRud
+router.get();
+
+// crUd
+router.put();
+
+// cruD
+router.delete();
+
+module.exports = router;
---
+++ routers/projectRouter.js
@@ -0,0 +1,26 @@
+const express = require("express");
+
+const Projects = require('../data/helpers/projectModel.js')
+
+const router = express.Router();
+
+// | Field       | Data Type | Metadata                                                                    |
+// | ----------- | --------- | --------------------------------------------------------------------------- |
+// | id          | number    | no need to provide it when creating projects, the database will generate it |
+// | name        | string    | required.                                                                   |
+// | description | string    | required.                                                                   |
+// | completed   | boolean   | used to indicate if the project has been completed, not required            |
+
+// Crud
+router.post();
+
+// cRud
+router.get();
+
+// crUd
+router.put();
+
+// cruD
+router.delete();
+
+module.exports = router;
